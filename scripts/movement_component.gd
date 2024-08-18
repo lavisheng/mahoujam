99ff027ebab4a07d14ef115a12dde4a2cc7361ce
lavisheng <hwangcodes@gmail.com>
2024-08-18 14:27:39 -0400

    Homing movement implementation

---
+++ scripts/movement_component.gd
@@ -0,0 +1,58 @@
+class_name MovementComponent
+extends Node2D
+@export var gravity_component: GravityComponent
+@export_subgroup("Settings")
+@export var ground_speed: float = 100
+@export var air_speed: float = 2000
+@export var homing_speed: float = 250
+const NUM_AIRDASHES = 2
+const NUM_HOMING = 1
+var num_airdashes: int = NUM_AIRDASHES
+var num_homing: int = NUM_HOMING
+var airdash_delta: float = .75
+var homing_delta: float = 2.
+
+
+func handle_horizontal_movement(body: CharacterBody2D, direction: float) -> void:
+    if not gravity_component.is_falling:
+        body.velocity.x = direction * ground_speed
+
+
+func handle_air_dash(body: CharacterBody2D, direction: float, delta: float) -> void:
+    if not body.is_on_floor() and direction != 0 and num_airdashes > 0:
+        body.velocity.x = direction * air_speed
+        gravity_component.airdash = true
+        body.velocity.y = 0
+        num_airdashes -= 1
+        airdash_delta = .75
+    elif gravity_component.airdash and airdash_delta > 0:
+        airdash_delta -= delta
+        body.velocity.x *= airdash_delta * airdash_delta * airdash_delta
+        if abs(body.velocity.x) <= 0.05:
+            gravity_component.airdash = false
+
+
+func landed(body: CharacterBody2D) -> void:
+    if body.is_on_floor():
+        body.transform = body.transform.looking_at(body.transform.get_origin() + Vector2(1.0, 0.0))
+        num_airdashes = NUM_AIRDASHES
+        num_homing = NUM_HOMING
+        homing_delta = 2.
+        gravity_component.homing = false
+
+
+func handle_homing_dash(
+    body: CharacterBody2D, target: Transform2D, delta: float, event: bool
+) -> void:
+    if event and not gravity_component.airdash and gravity_component.is_falling and num_homing > 0:
+        print("HOMING")
+        gravity_component.homing = true
+        num_homing -= 1
+    if gravity_component.homing and homing_delta > 0:
+        body.transform = body.transform.looking_at(target.get_origin())
+        body.velocity.x = homing_speed * cos(body.transform.get_rotation())
+        body.velocity.y = homing_speed * sin(body.transform.get_rotation())
+        homing_delta -= delta
+    else:
+        homing_delta = 2.
+        gravity_component.homing = false
---
+++ scripts/player.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+@export_subgroup("Nodes")
+@export var gravity_component: GravityComponent
+@export var input_component: InputComponent
+@export var movement_component: MovementComponent
+@export var jump_component: JumpComponent
+#@export var homing_target :     Node
+var bullets = 1
+var bullet = null
+
+
+#var bullet
+func fire():
+    if bullets > 0:
+        bullet = load("res://scenes/player_bullet.tscn").instantiate()
+        bullet.transform = bullet.transform.translated(self.transform.get_origin())
+        get_parent().add_child(bullet)
+        bullets -= 1
+
+
+func _physics_process(delta):
+    gravity_component.handle_gravity(self, delta)
+    movement_component.handle_horizontal_movement(self, input_component.input_horizontal)
+    movement_component.handle_air_dash(self, input_component.input_doubletap, delta)
+    if input_component.get_fire_input():
+        fire()
+    if bullet != null:
+        movement_component.handle_homing_dash(
+            self, bullet.transform, delta, input_component.get_homing_input()
+        )
+    jump_component.handle_jump(self, input_component.get_jump_input())
+    input_component.input_doubletap = 0
+    movement_component.landed(self)
+    move_and_slide()
