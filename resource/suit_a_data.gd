0e47beefe9218783c42d85644960b500132d468e
ChamplainJamesG <35664326+ChamplainJamesG@users.noreply.github.com>
2024-08-20 22:21:46 -0700

    Add collectable logic
    Add very basic collectable logic

--- resource/suit_a_data.gd
+++ resource/suit_a_data.gd
@@ -13,25 +13,25 @@ var shoot_index = 3
 
 
 func SuitAbilityCallback(body: CharacterBody2D, wantInput: bool):
-    if wantInput and not body.is_on_floor() and shoot_index == 3:
-        hover_time = HOVER_TIME
-        shoot_index = 0
-        for i in range(3):
-            bullets[i] = load("res://scenes/components/suite_a_bullet.tscn").instantiate()
-            bullets[i].transform = bullets[i].transform.translated(
-                body.transform.get_origin() + OFFSET[i]
-            )
-            body.get_parent().add_child(bullets[i])
-        body.velocity = Vector2(0, 0)
-    #do a giga jump ( of hell )
-    #if wantInput and body.is_on_floor():
-    #    body.velocity.y = giga_jump_vel
-    print("I'm an overloaded suit a")
+	if wantInput and not body.is_on_floor() and shoot_index == 3:
+		print("I'm an overloaded suit a");
+		hover_time = HOVER_TIME
+		shoot_index = 0
+		for i in range(3):
+			bullets[i] = load("res://scenes/components/suite_a_bullet.tscn").instantiate()
+			bullets[i].transform = bullets[i].transform.translated(
+				body.transform.get_origin() + OFFSET[i]
+			)
+			body.get_parent().add_child(bullets[i])
+		body.velocity = Vector2(0, 0)
+	#do a giga jump ( of hell )
+	#if wantInput and body.is_on_floor():
+	#    body.velocity.y = giga_jump_vel
 
 func SuitAbilityProcess(body: CharacterBody2D, wantInput: bool, delta: float):
-    if hover_time > 0 and shoot_index < 3:
-        hover_time -= delta
-        body.velocity.y = 0
-        if wantInput:
-            bullets[shoot_index].fire_bullet()
-            shoot_index += 1
+	if hover_time > 0 and shoot_index < 3:
+		hover_time -= delta
+		body.velocity.y = 0
+		if wantInput:
+			bullets[shoot_index].fire_bullet()
+			shoot_index += 1
