37ab0635d62dc109eb53ee5d65eef21c666c4ee2
lavisheng <hwangcodes@gmail.com>
2024-08-27 01:03:34 -0400

    Collisions for bullet on enemy enabled

--- scripts/suit_component.gd
+++ scripts/suit_component.gd
@@ -2,17 +2,17 @@ class_name SuitComponent extends Node
 
 
 func ActivatePower(player: Player, suit: SuitData) -> void:
-    suit.SuitAbilityCallback(player)
+	suit.SuitAbilityCallback(player)
 
 
 func ProcessPower(player: Player, suit: SuitData, delta: float) -> void:
-    suit.SuitAbilityProcess(player, delta)
+	suit.SuitAbilityProcess(player, delta)
 
 
 func ProcessSuitSwap(
-    body: Player, primarySuit: SuitData, secondarySuit: SuitData, inputPressed: bool
+	body: Player, primarySuit: SuitData, secondarySuit: SuitData, inputPressed: bool
 ) -> void:
-    if inputPressed:
-        body.active_suit = secondarySuit
-        body.inactive_suit = primarySuit
-        body.modulate = primarySuit.suitColor
+	if inputPressed:
+		body.active_suit = secondarySuit
+		body.inactive_suit = primarySuit
+		body.modulate = primarySuit.suitColor
--- scripts/suite_a_bullet.gd
+++ scripts/suite_a_bullet.gd
@@ -1,19 +1,23 @@
 class_name SuitABullet
-extends Sprite2D
+extends Area2D
 var travel_speed: float = 1000
 var fire: bool = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    pass  # Replace with function body.
+	pass  # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-    if fire:
-        position.x += travel_speed * delta
+	if fire:
+		position.x += travel_speed * delta
+
+
+func _on_body_entered(body):
+	print("COLLIDE")
 
 
 func fire_bullet() -> void:
-    fire = true
+	fire = true
