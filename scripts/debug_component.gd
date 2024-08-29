62a9c7353dcc20850deafe8f035a706e459ece98
ChamplainJamesG <35664326+ChamplainJamesG@users.noreply.github.com>
2024-08-29 00:01:12 -0700

    Add Debug component & UI for combo testing
    Add Debug component for debugging purposes.
    Update input & player to handle that
    Add to game hud & combo manager
    Hook up UI to combo manager for HuD purposes

---
+++ scripts/debug_component.gd
@@ -0,0 +1,19 @@
+class_name DebugComponent extends Node
+
+@export var comboManagerNode: ComboManager
+
+# Called when the node enters the scene tree for the first time.
+# func _ready() -> void:
+# 	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+# func _process(delta: float) -> void:
+# 	pass
+
+
+func DebugCombo(inputPressed: bool) -> void:
+	if inputPressed:
+		var node = comboManagerNode
+		if not node:
+			node = $root/Game/ComboManager
+		node.ModifyCombo(false)
