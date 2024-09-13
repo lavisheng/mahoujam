f99d4fab21ba4876f78d1c621827519ecec60f6b
ChamplainJamesG <35664326+ChamplainJamesG@users.noreply.github.com>
2024-09-12 23:29:30 -0700

    Add Death Screen

--- scripts/UI/main_scene.gd
+++ scripts/UI/main_scene.gd
@@ -11,3 +11,9 @@ func _process(delta: float) -> void:
 
 func StartGame() -> void:
 	get_tree().change_scene_to_file( "res://scenes/Levels/Level_1.tscn" );
+
+func GoToMainScene() -> void:
+	get_tree().change_scene_to_file( "res://scenes/UI/main_scene.tscn" );
+
+func Exit() -> void:
+	get_tree().quit();
